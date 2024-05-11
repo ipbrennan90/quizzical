035dda8b046fa5fd79e64d907aa6056fb81a6541
Ian Brennan <ian@convertkit.com>
2024-05-11 17:09:39 -0400

    Chat history with redis working

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,9 +1,14 @@
 from . import api_blueprint
 from flask import request, jsonify
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
-from operator import itemgetter
+from langchain_community.chat_message_histories import RedisChatMessageHistory
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain.schema import StrOutputParser
+
+from operator import itemgetter
+import os
 
 from app.services.resource_loader import ResourceLoader
 from app.services.embeddings import Embeddings
@@ -23,26 +28,31 @@ def handle_query():
 
     return jsonify({ "data": { "namespace": namespace}, "status": { "code": 200, "message": "success"}})
 
-@api_blueprint.route('/query', methods=['POST'])
-def search():
+@api_blueprint.route('/chat', methods=['POST'])
+def chat():
     request_data = request.get_json()
-    query = request_data.get('query')
+    query = request_data.get('question')
     namespace = request_data.get('namespace')
     character = request_data.get('character')
+    chat_session_id = request_data.get('chat_session_id')
     prompt_to_use = prompt(character)
     vector_store = VectorStore(namespace=namespace)
     retriever = vector_store.retriever()
-    prompt_template = ChatPromptTemplate.from_template(prompt_to_use)
+    prompt_template = ChatPromptTemplate.from_messages([("system", prompt_to_use), MessagesPlaceholder(variable_name="history"), ("human", "{question}")])
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, streaming=True)
-    retrieval_augmented_chain = { "context": itemgetter("question") | retriever, "question": itemgetter("question") } | prompt_template | llm | StrOutputParser()
-    response = retrieval_augmented_chain.invoke({"question": query})
+
+    get_context = itemgetter("question") | retriever
+    first_step = RunnablePassthrough.assign(context=get_context)
+    retrieval_augmented_chain = first_step | prompt_template | llm | StrOutputParser()
+    chain_with_history = RunnableWithMessageHistory(
+        retrieval_augmented_chain,
+        lambda session_id: RedisChatMessageHistory(session_id, url=os.getenv("REDIS_URL")),
+        input_messages_key="question",
+        history_messages_key="history"
+    )
+
+    config = {"configurable": {"session_id": chat_session_id}}
+
+    response = chain_with_history.invoke({"question": query}, config=config)
 
     return jsonify({ "data": response, "status": { "code": 200, "message": "success"}})
-
-
-
-
-
-
-
-    return jsonify({"data": "searching..."})
--- app/models/prompts/steve_brule.py
+++ app/models/prompts/steve_brule.py
@@ -5,11 +5,9 @@ class SteveBrulePrompt():
     def prompt(self):
       return """You are a teacher that is trying to explain the concepts included in the text given in the context.
       You have the personality of Dr. Steve Brule from the TV series Tim and Eric's Awesome show, Great Job!
-      When you explain the concepts, you must use the same language and tone as Dr. Steve Brule.
-
-      Context:
-      {context}
-
-      Question:
-      {question}
+      When you explain the concepts, you must use the same language and tone as Dr. Steve Brule. If you don't find the answer
+      within the given context answer the question with a steve brule-ism that tells the user to stay focused on studying the text.
+      Context: {context}
+      Question: {question}
+      History: {history}
       """
